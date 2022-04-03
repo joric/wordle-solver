f0873fdd5de629a4713e6a6ed3536c986a0a4780
joric <joric@users.noreply.github.com>
2022-04-04 04:10:15 +0500

    updated files

--- solver.py
+++ solver.py
@@ -2,15 +2,19 @@
 
 import re
 
-# taken from https://github.com/shafirov/wordleSolver
+# some parts are taken from https://github.com/potassium-chloride/WordleSolver
 
 def isThisSecretAvailable(testword,mask,secret):
     if len(mask) != len(secret) or len(testword)!=len(secret):
         return False
+    dup = set(testword[i] for i in range(len(mask)) if mask[i]=='G' or mask[i]=='Y')
     for i in range(len(mask)):
-        if(mask[i]=='N' and testword[i] not in secret):continue
-        if(mask[i]=='G' and testword[i]==secret[i]):continue
-        if(mask[i]=='Y' and testword[i] in secret and testword[i]!=secret[i]):continue
+        if mask[i]=='N' and (testword[i] not in secret or testword[i] in dup):
+            continue
+        if mask[i]=='G' and testword[i]==secret[i]:
+            continue
+        if mask[i]=='Y' and testword[i] in secret and testword[i]!=secret[i]:
+            continue
         return False
     return True
 
@@ -29,8 +33,12 @@ def solve(*args):
         testword = [c.strip('[]()') for c in m]
         mask = ['G' if '[' in c else 'Y' if '(' in c else 'N' for c in m]
         newwordlist = getAvailableWordsByMask(testword, mask, newwordlist)
+        #print(testword, mask, newwordlist)
     return newwordlist[:50]
 
 if __name__ == "__main__":
-    print(solve('н(о)рк[а]','гли(с)(т)','музей')) # стопа
-    print(solve('но(р)ка','г[л]ист','муз[е]й')) # плеер
+    test = lambda x: print(solve(*x.split()))
+    test('н(о)рк[а] гли(с)(т) музей') # стопа
+    test('но(р)ка г[л]ист муз[е]й') # плеер
+    test('нор[к][а] гли(с)т фа(с)[к][а]') # сушка
+    test('но(р)ка глист муз(е)й [д]ожд[ь]') # дверь
